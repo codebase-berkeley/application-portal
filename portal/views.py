8439256b5473dc011aa3a19fe699dcf98cc81274
Elise Xu <elisexu98@gmail.com>
2017-10-17 16:39:56 -0700

    adding database querying

--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from portal.models import Category, Application
 # Create your views here.
 
 def advanced_hello(request, first_name):
     return render(request, "portal/hello.html", { "first_name": first_name })
+
+def testcategories(request):
+	listy = list(Category.objects.all())
+	apps = list(Application.objects.all())
+	return render(request, "portal/testcategories.html", {'categories': listy, 'apps': apps})
+
+
