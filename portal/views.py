80aeac60297dd6ac69ed16af4e910678050af342
Amitav Baruah <abaruah@berkeley.edu>
2017-10-17 11:12:43 -0700

    Functionality done, needs formatting

--- portal/views.py
+++ portal/views.py
@@ -7,9 +7,10 @@ def advanced_hello(request, first_name):
     return render(request, "portal/hello.html", { "first_name": first_name })
 def render_app(request,app_pk):
     application = Application.objects.get(pk = app_pk)
-    first_name = application.first_name
-    last_name = application.last_name
-    email = application.email
-    questions = Question.objects.values()
-    application = str(Comment.objects.values())
-    return render(request, "portal/hello.html", {"first_name": questions.get(id = 1) })
+    first_name, last_name, email = application.first_name, application.last_name, application.email
+    questions = Question.objects.order_by('order_number')
+    question_texts = ["<"+question.question_type+">: "+str(question) for question in questions]
+    answers = [answer.answer_text for answer in application.answer_set.all()]
+    return render(request, "test/test.html",
+{"first_name":first_name, "last_name":last_name, "email":email,
+    "questions": question_texts, "answers": answers if answers else "ERROR NO ANSWERS" })
