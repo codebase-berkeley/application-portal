99fe0eb656eefabd43d02acc5df07f7222d6de7a
Amitav Baruah <abaruah@berkeley.edu>
2017-10-17 10:00:24 -0700

    added url/view

--- portal/urls.py
+++ portal/urls.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'hello/(?P<first_name>[a-zA-Z]+)$', views.advanced_hello, name="advanced_hello")
+    url(r'hello/(?P<first_name>[a-zA-Z]+)$', views.advanced_hello, name="advanced_hello"),
+    url(r'application/(?P<application_id>[0-9]*$)',views.render_app, name = "get_application"),
 ]
