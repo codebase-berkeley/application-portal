2610acfab0be3933e62e548849a5412f451dc58c
Amitav Baruah <abaruah@berkeley.edu>
2017-10-17 10:24:44 -0700

    update views

--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from portal.models import *
 # Create your views here.
 
 def advanced_hello(request, first_name):
     return render(request, "portal/hello.html", { "first_name": first_name })
+def render_app(request,application_id):
+    application = Application.objects.get(id = application_id)
+    first_name = application.first_name
+    last_name = application.last_name
+    email = application.email
+    questions = Question.objects.values()
+    application = str(Comment.objects.values())
+    return render(request, "portal/hello.html", {"first_name": questions })
