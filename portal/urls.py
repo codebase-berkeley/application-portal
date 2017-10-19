9ee8299947b3682054cff788af2d848e24754b4b
Nilay Khatore <khatorenilay@gmail.com>
2017-10-18 20:38:05 -0700

    new form stuff

--- portal/urls.py
+++ portal/urls.py
@@ -3,5 +3,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'hello/(?P<first_name>[a-zA-Z]+)$', views.advanced_hello, name="advanced_hello")
+    url(r'hello/(?P<first_name>[a-zA-Z]+)$', views.advanced_hello, name="advanced_hello"),
+    url(r'/edit/form', views.form, name='form'),
+    url(r'/edit/form/q/<q_pk>', views.add_question, name='add_question')
 ]
--- portal/views.py
+++ portal/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def advanced_hello(request, first_name):
     return render(request, "portal/hello.html", { "first_name": first_name })
+
+def form(request):
+
+def add_question(request):
+
+def delete_question(request):
+
+def edit_question(request):    
