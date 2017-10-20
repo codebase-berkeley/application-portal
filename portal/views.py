556aeea37d41a089ab6ade88a1150e480ef21062
Amitav Baruah <abaruah@berkeley.edu>
2017-10-20 16:14:19 -0700

    Repathed css and html, starting to soft code

--- portal/views.py
+++ portal/views.py
@@ -11,6 +11,6 @@ def render_app(request,app_pk):
     questions = Question.objects.order_by('order_number')
     question_texts = ["<"+question.question_type+">: "+str(question) for question in questions]
     answers = [answer.answer_text for answer in application.answer_set.all()]
-    return render(request, "test/test.html",
+    return render(request, "portal/application.html",
 {"first_name":first_name, "last_name":last_name, "email":email,
     "questions": question_texts, "answers": answers if answers else "ERROR NO ANSWERS" })
