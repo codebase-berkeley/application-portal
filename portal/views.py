52b385f03958c0c74291f925df31daeec6d2d45d
mhwangc <mhwangc@berkeley.edu>
2017-10-21 00:34:51 -0700

    views

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from portal.models import Question
 
 # Create your views here.
 
@@ -7,18 +8,18 @@ def advanced_hello(request, first_name):
     return render(request, "portal/hello.html", { "first_name": first_name })
 
 def form(request):
-    return render(request, "portal/question_form/edit_form.html", { "questions": Question.objects.all() })
+    return render(request, "portal/question_forms/edit_form.html", { "questions": Question.objects.all() })
 
 def create_question(request, q_text, q_type):
     new_question = Question(question_text=q_text, question_type=q_type)
     new_question.save()
-    return render(request, "portal/question_form/edit_form.html", { "questions": Question.objects.all() })
+    return render(request, "portal/question_forms/edit_form.html", { "questions": Question.objects.all() })
 
 def delete_question(request, question):
     question.delete()
-    return render(request, "portal/question_form/edit_form.html", { "questions": Question.objects.all() })
+    return render(request, "portal/question_forms/edit_form.html", { "questions": Question.objects.all() })
 
 def edit_question(request, question, new_text):
     question.question_type = new_text
     question.save()
-    return render(request, "portal/question_form/edit_form.html", { "questions": Question.objects.all() })
+    return render(request, "portal/question_forms/edit_form.html", { "questions": Question.objects.all() })
