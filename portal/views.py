a6961cddbbb422c453e7fff3d04c13108b38bfa8
mhwangc <mhwangc@berkeley.edu>
2017-10-22 17:55:09 -0700

    edit functionality

--- portal/views.py
+++ portal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from portal.models import Question
 import ast
@@ -32,13 +33,22 @@ def delete_question(request, question):
 
 
 def edit_question(request, pk=''):
+    question = Question.objects.get(pk=pk)
+    options = None
+    if question.options:
+        options = ast.literal_eval(question.options)
     if request.method == "GET":
-        question = Question.objects.get(pk=pk)
-        options = None
-        if question.options:
-            options = ast.literal_eval(question.options)
         return render(request, "portal/question_forms/edit_question.html", { "question": question, "options": options})
-    question.question_text = new_text
+    question.question_text = request.POST['question_text']
+    if options:    
+        for option in options:
+            if request.POST.get(option, False):
+                options.remove(option)
+    if request.POST.get("add_options", False):
+        new_options = request.POST["add_options"]
+        new_options = [x.strip() for x in new_options.split(',')]
+        options.extend(new_options)
+    question.options = options
     question.save()
-    return render(request, "portal/question_forms/edit_form.html", { "questions": Question.objects.all() })
+    return redirect('portal:form')
 
