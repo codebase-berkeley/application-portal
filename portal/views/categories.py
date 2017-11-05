d3a7e2283c8909f9adb112911611ea89cbd336d5
Raymond Guo <guor.lei@berkeley.edu>
2017-11-04 18:49:28 -0700

    Added API functionality, error with send_massemail url (can't be found???)

--- portal/views/categories.py
+++ portal/views/categories.py
@@ -5,9 +5,14 @@ from portal.models import *
 from portal.models import Question, Answer
 from portal.models import Category, Application
 import ast
+import json, requests
 
 from django.utils.timezone import localtime, now
 from django.contrib.auth.decorators import login_required
+
+URL = "https://us17.api.mailchimp.com/3.0/"
+KEY = "e1a1bff19a0cecf9447238a56a1efa9f"
+
 
 
 def create_category(request):
@@ -66,7 +71,7 @@ def change_category(request, app_pk):
     application.category.name = request.POST['category']
     return redirect('portal:get_application', app_pk)
 
-def send_massemail(request, pk=''):
+def create_massemail(request, pk=''):
     category = Category.objects.get(pk=pk)
     list_app = list(Application.objects.all())
     category_apps = []
@@ -74,3 +79,34 @@ def send_massemail(request, pk=''):
         if app.category == category:
             category_apps.append(app)
     return render(request, "portal/massemail.html", {"category_apps": category_apps})
+
+def send_massemail(request, category_apps):
+    subject = request.POST["subject"]
+    body = request.POST["body"]
+
+
+    create_list_params = {"name": "name", "contact": {"company": "CodeBase", "address1": "2650 Haste Street", "city": "Berkeley", "state": "CA", "zip": "94720", "country": "USA"}, "permission_reminder": "You're receiving this email because you applied to CodeBase.", "campaign_defaults": {"from_name": "CodeBase", "from_email": "contact@codebase.berkeley.edu", "subject": subject, "language": "en"}, "email_type_option": True}
+
+
+    create_list = requests.post(url=URL+"/lists/"+KEY, params = create_list_params)
+    obj = json.loads(create_list.text)
+    list_id = obj['id']
+
+    for app in category_apps:
+        add_member_params = {"email_address": app.email, "status": "subscribed"}
+        requests.post(url=URL+"/lists/"+list_id+"/members/"+KEY, params = add_member_params)
+
+    
+    create_campaign_params = {"type": "regular", "recipients":{"list_id": list_id}}
+    create_campaign = requests.post(url=URL+"/campaigns/"+KEY, params = create_campaign_params)
+    obj = json.loads(create_campaign.text)
+    campaign_id = obj['id']
+
+
+    create_email_params = {"plaintext": body}
+    requests.put(url=URL+"/campaigns/"+campaign_id+"/content/"+KEY, params = create_email_params)
+
+    requests.post(url=URL+"/campaigns/"+campaign_id+"/actions/send/"+KEY, params={})
+
+    return redirect('portal:show_category', category_apps[0].category.pk)
+
