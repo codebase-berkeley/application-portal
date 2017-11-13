9a335f1beddc658bf08fdeac7d855561507d2c93
Brian DeLeonardis <bdeleonardis1@gmail.com>
2017-11-12 20:07:25 -0800

    Add assignment fields

--- portal/admin.py
+++ portal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Application, Comment, Paragraph, Checkbox, Radiobutton, Dropdown, Answer
+from .models import Category, Application, Comment, Paragraph, Checkbox, Radiobutton, Dropdown, Answer, Assignment
 
 admin.site.register(Category)
 admin.site.register(Application)
@@ -9,3 +9,4 @@ admin.site.register(Checkbox)
 admin.site.register(Radiobutton)
 admin.site.register(Dropdown)
 admin.site.register(Answer)
+admin.site.register(Assignment)
