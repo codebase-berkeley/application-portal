8a31ac06ef48587544f74e00bd192ee034db10da
Mary Xu <xu.ruili99@gmail.com>
2017-11-16 22:26:34 -0800

    Delete assignment.py

--- portal/views/assignment.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.decorators import login_required
-@login_required
-def show_assignment(request, pk=''):
-    context = get_dashboard_context(request.user.username, request.user.email)
-    list_app = list(request.user.assignment.applicant.all())
-    context["list_app"] = list_app
-    return render(request, "portal/myassignments.html", context)
