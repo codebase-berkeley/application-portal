8498189844cfaf4a06ef61bf3fba141e8ea58a82
Mary <xu.ruili99@gmail.com>
2017-11-16 22:09:15 -0800

    views function and html for myassignments

--- assignment.py
+++
@@ -1,6 +0,0 @@
-@login_required
-def show_assignment(request, pk=''):
-    context = get_dashboard_context(request.user.username, request.user.email)
-    list_app = list(request.user.assignment.applicant.all())
-    context["list_app"] = list_app
-    return render(request, "portal/myassignments.html", context)
